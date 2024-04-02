beb3d2545e6be5bd3891672cdb08a4ef9cf94ebf
hoang_vh24 <136774878+hoangvhgch220975@users.noreply.github.com>
2024-04-02 11:41:15 +0700

    Add files via upload

---
+++ HTML/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./D:\CODE\CO1201\HTML\Chị Thoa xinh gái/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
